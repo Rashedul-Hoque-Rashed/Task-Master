31d3a32b1048dddaa241090c94798d32cf5ffbfe
Rashed <rashedulhoqueceo@gmail.com>
2024-02-12 01:09:37 +0600

    create update status ongoing and completed

--- src/Components/UpdateTask.jsx
+++ src/Components/UpdateTask.jsx
@@ -1,26 +1,26 @@
-import { useContext } from "react";
-import { AuthContext } from "../Provider/AuthProvider";
-import { IoMdCloseCircle } from "react-icons/io";
-import { GoArrowUpRight } from "react-icons/go";
-import Swal from "sweetalert2";
-import { CiEdit } from "react-icons/ci";
+import { useRef } from 'react';
+import { CiEdit } from 'react-icons/ci';
+import { GoArrowUpRight } from 'react-icons/go';
+import { IoMdCloseCircle } from 'react-icons/io';
 
-
-const UpdateTask = ({taskId}) => {
-    const { user } = useContext(AuthContext);
+const UpdateTask = ({ id, tasked }) => {
+    const titleRef = useRef(null);
+    const deadlineRef = useRef(null);
+    const priorityRef = useRef(null);
+    const descriptionRef = useRef(null);
+    
 
     const taskDataString = localStorage.getItem('tasked');
     const taskData = taskDataString ? JSON.parse(taskDataString) : [];
 
     const handelEdit = (e) => {
         e.preventDefault();
-        const form = e.target;
-        const title = form.title.value;
-        const deadline = form.deadline.value;
-        const priority = form.priority.value;
-        const description = form.description.value;
+        const title = titleRef.current.value;
+        const deadline = deadlineRef.current.value;
+        const priority = priorityRef.current.value;
+        const description = descriptionRef.current.value;
 
-        const taskIndex = taskData.findIndex(task => task.id === taskId);
+        const taskIndex = taskData.findIndex(task => task.id === id);
 
         if (taskIndex !== -1) {
             // Modify the task data
@@ -30,20 +30,18 @@ const UpdateTask = ({taskId}) => {
             taskData[taskIndex].description = description;
 
             // Store the updated data back into localStorage
-            localStorage.setItem('tasks', JSON.stringify(taskData));
+            localStorage.setItem('tasked', JSON.stringify(taskData));
+            console.log('Task updated successfully');
+            window.location.reload();
         } else {
             console.error('Task not found.'); // Handle error if task not found
         }
-    }
-
+    };
 
     return (
         <div className="mt-2">
             <div>
-                <button onClick={() => document.getElementById('my_modal_5').showModal()}
-                    type="button"
-                    className="active:scale-95"
-                >
+                <button onClick={() => document.getElementById('my_modal_5').showModal()} type="button" className="active:scale-95">
                     <CiEdit className='w-8 h-8 bg-gray-600 rounded-xl text-white p-1' />
                 </button>
             </div>
@@ -55,23 +53,23 @@ const UpdateTask = ({taskId}) => {
                         </form>
                         <form onSubmit={handelEdit} className="w-full mx-auto flex flex-col">
                             <label htmlFor="title" className="mx-2 text-lg font-semibold">Title</label>
-                            <input placeholder="Enter Task Title" name="title" id="title" className="input input-bordered rounded-full w-full mt-2 mb-4 py-2 px-4" required />
+                            <input ref={titleRef} placeholder="Enter Task Title" name="title" id="title" defaultValue={tasked.title} className="input input-bordered rounded-full w-full mt-2 mb-4 py-2 px-4" required />
 
                             <label htmlFor="deadline" className="mx-2 text-lg font-semibold">Deadline</label>
-                            <input type="date" name="deadline" min={new Date().toISOString().split('T')[0]} id="deadline" className="input input-bordered rounded-full w-full mt-2 mb-4 py-2 px-4"  required />
+                            <input type="date" ref={deadlineRef} name="deadline" min={new Date().toISOString().split('T')[0]} id="deadline" defaultValue={tasked.deadline} className="input input-bordered rounded-full w-full mt-2 mb-4 py-2 px-4"  required />
 
                             <label htmlFor="priority" className="mx-2 text-lg font-semibold">Priority</label>
-                            <select id="priority" name="priority" className="select select-bordered rounded-full w-full mt-2 mb-4 py-2 px-4" required>
+                            <select ref={priorityRef} id="priority" defaultValue={tasked.priority} name="priority" className="select select-bordered rounded-full w-full mt-2 mb-4 py-2 px-4" required>
                                 <option value="High">High</option>
                                 <option value="Medium">Medium</option>
                                 <option value="Low">Low</option>
                             </select>
 
                             <label htmlFor="description" className="mx-2 text-lg font-semibold">Description</label>
-                            <textarea type="text" name="description" rows={30} cols={10} placeholder="Enter Task Description" id="description" className="input input-bordered h-28 py-2 px-4 rounded-3xl w-full mt-2 mb-4" required />
+                            <textarea ref={descriptionRef} type="text" name="description" rows={30} cols={10} placeholder="Enter Task Description" defaultValue={tasked.description} id="description" className="input input-bordered h-28 py-2 px-4 rounded-3xl w-full mt-2 mb-4" required />
 
                             <button type="submit" className="text-base font-bold bg-none w-fit mx-auto text-[#00bbc9] border-2 border-[#00bbc9] px-4 py-2 mt-6 rounded-xl hover:text-white hover:bg-[#00bbc9] flex items-center gap-2 active:scale-95">
-                                Add Task <GoArrowUpRight className="h-6 w-6" />
+                                Update Task <GoArrowUpRight className="h-6 w-6" />
                             </button> 
                         </form>
                     </div>
--- src/Pages/TaskManagement/TaskManagement.jsx
+++ src/Pages/TaskManagement/TaskManagement.jsx
@@ -12,8 +12,8 @@ const TaskManagement = () => {
 
 
     const todoTasks = taskData.filter(task => task.status === 'To-Do');
-    // const ongoingTasks = taskData.filter(task => task.status === 'Ongoing');
-    // const completedTasks = taskData.filter(task => task.status === 'Completed');
+    const ongoingTasks = taskData.filter(task => task.status === 'Ongoing');
+    const completedTasks = taskData.filter(task => task.status === 'Completed');
 
     const handleDelete = (id) => {
 
@@ -40,13 +40,30 @@ const TaskManagement = () => {
             })
     }
 
+    const handelStatus = (id, status) => {
+
+        const taskIndex = taskData.findIndex(task => task.id === id);
+
+        if (taskIndex !== -1) {
+            taskData[taskIndex].status = status;
+
+            localStorage.setItem('tasked', JSON.stringify(taskData));
+            console.log('Task updated successfully');
+            window.location.reload();
+        } else {
+            console.error('Task not found.'); 
+        }
+        
+           
+    }
+
 
     return (
         <div>
             <div className="flex justify-between gap-6 flex-col lg:flex-row mx-4">
-                <div className="flex-1 bg-base-300 min-h-screen px-4">
+                <div className="flex-1 bg-base-300 min-h-screen px-4 mt-16 lg:mt-0">
                     <h4 className="text-center text-xl font-bold mt-6 mb-2">To-Do</h4>
-                    <div className="border-4 border-red-400 rounded-full"></div>
+                    <div className="border-4 border-[#FF6347] rounded-full"></div>
                     <div>
                         <AddTask />
                         {
@@ -62,11 +79,19 @@ const TaskManagement = () => {
                                             <p className={`text-base font-medium text-white px-4 py-1 w-fit rounded-full ${task.priority === 'High' ? 'bg-[#FFA500]' : task.priority === 'Medium' ? 'bg-[#0000FF]' : 'bg-[#808080]'}`}>{task.priority}</p>
                                         </div>
                                         <div className='flex gap-4 items-center'>
-                                            <UpdateTask taskId={task.id} />
+                                            <UpdateTask id={task.id} tasked={task} />
                                             <button onClick={() => handleDelete(task.id)} className='active:scale-95'>
                                                 <MdAutoDelete className='w-8 h-8 bg-red-700 rounded-xl text-white p-1' />
                                             </button>
                                         </div>
+                                    </div>
+                                    <div className='flex gap-2 items-center border py-1 px-4 w-fit rounded-full'>
+                                    <button onClick={() => handelStatus(task.id, 'Ongoing')} className="text-base font-medium text-white px-4 py-2 bg-[#3CB371] w-fit rounded-full mr-2 active:scale-95">
+                                            Ongoing
+                                        </button>
+                                        <button onClick={() => handelStatus(task.id, 'Completed')} className="text-base font-medium text-white px-4 py-2 bg-[#9370DB] w-fit rounded-full active:scale-95">
+                                            Completed
+                                        </button>
                                     </div>
                                 </div>
                             </div>)
@@ -75,16 +100,74 @@ const TaskManagement = () => {
                 </div>
                 <div className="flex-1 bg-base-300 min-h-screen px-4">
                     <h4 className="text-center text-xl font-bold mt-6 mb-2">Ongoing</h4>
-                    <div className="border-4 border-amber-400 rounded-full"></div>
+                    <div className="border-4 border-[#3CB371] rounded-full"></div>
                     <div>
-                        {/* ongoing data */}
+                    {
+                            ongoingTasks.map(task => <div key={task.id} className="card bg-base-100 shadow-xl my-4 cursor-pointer">
+                                <div className="card-body">
+                                    <div className="flex justify-between flex-col">
+                                        <h2 className="card-title text-[#222E48] text-2xl font-bold">{task.title}</h2>
+                                        <h4 className="text-[#222e48] text-base font-medium">Deadline: {task.deadline}</h4>
+                                    </div>
+                                    <p className="text-[#222E48] my-2 text-sm w-64">{task.description}</p>
+                                    <div className="flex flex-col md:flex-row items-center justify-between">
+                                        <div>
+                                            <p className={`text-base font-medium text-white px-4 py-1 w-fit rounded-full ${task.priority === 'High' ? 'bg-[#FFA500]' : task.priority === 'Medium' ? 'bg-[#0000FF]' : 'bg-[#808080]'}`}>{task.priority}</p>
+                                        </div>
+                                        <div className='flex gap-4 items-center'>
+                                            <UpdateTask id={task.id} tasked={task} />
+                                            <button onClick={() => handleDelete(task.id)} className='active:scale-95'>
+                                                <MdAutoDelete className='w-8 h-8 bg-red-700 rounded-xl text-white p-1' />
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className='flex gap-2 items-center border py-1 px-4 w-fit rounded-full'>
+                                    <button onClick={() => handelStatus(task.id, 'To-Do')} className="text-base font-medium text-white px-4 py-2 bg-[#FF6347] w-fit rounded-full mr-2 active:scale-95">
+                                            To-Do
+                                        </button>
+                                        <button onClick={() => handelStatus(task.id, 'Completed')} className="text-base font-medium text-white px-4 py-2 bg-[#9370DB] w-fit rounded-full active:scale-95">
+                                            Completed
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>)
+                        }
                     </div>
                 </div>
                 <div className="flex-1 bg-base-300 min-h-screen px-4">
                     <h4 className="text-center text-xl font-bold mt-6 mb-2">Completed</h4>
-                    <div className="border-4 border-green-400 rounded-full"></div>
+                    <div className="border-4 border-[#9370DB] rounded-full"></div>
                     <div>
-                        {/* completed data */}
+                    {
+                            completedTasks.map(task => <div key={task.id} className="card bg-base-100 shadow-xl my-4 cursor-pointer">
+                                <div className="card-body">
+                                    <div className="flex justify-between flex-col">
+                                        <h2 className="card-title text-[#222E48] text-2xl font-bold">{task.title}</h2>
+                                        <h4 className="text-[#222e48] text-base font-medium">Deadline: {task.deadline}</h4>
+                                    </div>
+                                    <p className="text-[#222E48] my-2 text-sm w-64">{task.description}</p>
+                                    <div className="flex flex-col md:flex-row items-center justify-between">
+                                        <div>
+                                            <p className={`text-base font-medium text-white px-4 py-1 w-fit rounded-full ${task.priority === 'High' ? 'bg-[#FFA500]' : task.priority === 'Medium' ? 'bg-[#0000FF]' : 'bg-[#808080]'}`}>{task.priority}</p>
+                                        </div>
+                                        <div className='flex gap-4 items-center'>
+                                            <UpdateTask id={task.id} tasked={task} />
+                                            <button onClick={() => handleDelete(task.id)} className='active:scale-95'>
+                                                <MdAutoDelete className='w-8 h-8 bg-red-700 rounded-xl text-white p-1' />
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className='flex gap-2 items-center border py-1 px-4 w-fit rounded-full'>
+                                        <button onClick={() => handelStatus(task.id, 'To-Do')} className="text-base font-medium text-white px-4 py-2 bg-[#FF6347] w-fit rounded-full active:scale-95">
+                                            To-Do
+                                        </button>
+                                    <button onClick={() => handelStatus(task.id, 'Ongoing')} className="text-base font-medium text-white px-4 py-2 bg-[#3CB371] w-fit rounded-full mr-2 active:scale-95">
+                                            Ongoing
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>)
+                        }
                     </div>
                 </div>
             </div>
@@ -94,3 +177,5 @@ const TaskManagement = () => {
 };
 
 export default TaskManagement;
+
+
